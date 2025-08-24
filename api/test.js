7dbdb3bc2c5bc4f1fa41fb7fd927dcc1d8e265a3
medusag2 <farruxashuraliyev62@gmail.com>
2025-08-24 20:34:22 -0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,9 +1,13 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const { name } = req.body || {};
-    res.status(200).json({ message: `Hello, ${name}! Test received.` });
-  } else {
-    res.status(403).json({ error: "Access Forbidden" });
+  if (req.method === "GET") {
+    return res.status(200).json({ message: "GET ishladi" });
   }
+
+  if (req.method === "POST") {
+    const { name } = req.body;
+    return res.status(200).json({ message: `Salom, ${name}! POST ishladi` });
+  }
+
+  return res.status(405).json({ error: "Method not allowed" });
 }
 
