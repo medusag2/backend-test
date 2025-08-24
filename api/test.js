6f827cfd6983fbed37ccbc4f598a4dd1451d9c0e
medusag2 <farruxashuraliyev62@gmail.com>
2025-08-24 18:52:13 -0100

    Create test.js

---
+++ api/test.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email } = req.body;
+    res.status(200).json({ message: `Hello, ${name}! Test received.` });
+  } else {
+    res.status(403).json({ error: 'Access Forbidden' });
+  }
+}
