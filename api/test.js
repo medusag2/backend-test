ff0b0c7aa2d5500e3d0b362e6570bf6262cefb54
medusag2 <farruxashuraliyev62@gmail.com>
2025-08-24 19:32:42 -0100

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const { name, email } = req.body;
+    const { farruh, farruxashuraliyev62@gmail.com } = req.body;
     res.status(200).json({ message: `Hello, ${name}! Test received.` });
   } else {
     res.status(403).json({ error: 'Access Forbidden' });
